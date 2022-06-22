5d0465b6ea9e9e36175810cf2c226f6788150eb9
«Alexey» <voropaev56@mail.ru>
2022-06-23 00:55:45 +0500

    Hello_Masha_if_Else

---
+++ Example_005_Hello_Masha/Program.cs
@@ -0,0 +1,14 @@
+﻿// Особенное приветствие для Маши
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, Машуля!!!))))");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
